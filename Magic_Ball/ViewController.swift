7688a94d0454a936ea9c33eb3750b5f8cc3338e9
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-09-17 16:23:17 +0300

    Randomization method added

--- Magic_Ball/ViewController.swift
+++ Magic_Ball/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func ButtonAction(_ sender: UIButton) {
+        let imageList = [
+            UIImage(named: "ball1"),
+            UIImage(named: "ball2"),
+            UIImage(named: "ball3"),
+            UIImage(named: "ball4"),
+            UIImage(named: "ball5"),
+        ]
+        
+        image.image = imageList.randomElement() as! UIImage
+        
+    }
+    
+    
 }
 
