676ba17f200e8e4127b23656d4cd5763831d18e4
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-09-17 15:42:58 +0300

    Initial Commit

---
+++ Magic_Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic_Ball
+//
+//  Created by Emirhan Serin on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
